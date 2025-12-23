8be7f4dcb7f7348347ec9b980198b84f59ca1c51
K-Jonin <jonin.0803@gmail.com>
2025-12-24 00:25:28 +0900

    UX改善

--- frontend/src/components/layout/Sidebar.tsx
+++ frontend/src/components/layout/Sidebar.tsx
@@ -6,8 +6,31 @@ interface SidebarProps {
   setIsSidebarOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 export const Sidebar: React.FC<SidebarProps> = (props: SidebarProps) => {
+  React.useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      if (event.key === 'Escape' && props.isSidebarOpen) {
+        props.setIsSidebarOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleEscKey);
+
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+    };
+  }, [props.isSidebarOpen, props.setIsSidebarOpen]);
+
   return (
     <>
+      {/* オーバーレイ */}
+      <div
+        className={`
+          ${styles.overlay}
+          ${props.isSidebarOpen ? styles.overlay__open : styles.overlay__close}
+        `}
+        onClick={() => props.setIsSidebarOpen(false)}
+      ></div>
+      {/* サイドバー */}
       <div
         className={`
         ${styles.sidebar}
